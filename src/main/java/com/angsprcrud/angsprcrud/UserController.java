59c37ce556e96ca41fa9d89c88a3693d00f1613b
sam kirk <samueljkirk10@gmail.com>
2019-02-16 13:08:56 +0000

    added basic controller

---
+++ src/main/java/com/angsprcrud/angsprcrud/UserController.java
@@ -0,0 +1,40 @@
+package com.angsprcrud.angsprcrud;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:4200", maxAge=3600)
+@RestController
+@RequestMapping({"/api"})
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @GetMapping(path = "/{id}")
+    public User findOne(@PathVariable("id") int id) {
+        return userService.findById(id);
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user) {
+        return userService.update(user);
+    }
+
+    @DeleteMapping
+    public User delete(PathVariable("id") int id){
+        return userService.delete(id);
+    }
+
+    @GetMapping
+    public List findAll() {
+        return userService.findAll();
+    }
+}
