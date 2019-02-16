8ec4b082bb9779751215760d973709871261ac38
sam kirk <samueljkirk10@gmail.com>
2019-02-16 13:17:32 +0000

    added service implimentation

---
+++ src/main/java/com/angsprcrud/angsprcrud/UserServiceImpl.java
@@ -0,0 +1,38 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepoistory;
+
+    @Override
+    public User create(User user) {
+        return userRepoistory.save(user);
+    }
+
+    @Override
+    public User delete(int id) {
+        User user = findById(id);
+        if(user != null){
+            repository.delete(user);
+        }
+        return user;
+    }
+
+    @Override
+    public List findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public User findById(int id) {
+        return repository.findOne(id);
+    }
+
+    @Override
+    public User update(User user) {
+        return null;
+    }
+}
