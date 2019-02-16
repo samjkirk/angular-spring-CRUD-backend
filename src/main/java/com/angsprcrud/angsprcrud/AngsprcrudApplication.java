06223271698317d0c5689862983ecae875109d03
sam kirk <samueljkirk10@gmail.com>
2019-02-16 12:46:12 +0000

    first commit

---
+++ src/main/java/com/angsprcrud/angsprcrud/AngsprcrudApplication.java
@@ -0,0 +1,14 @@
+package com.angsprcrud.angsprcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngsprcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngsprcrudApplication.class, args);
+	}
+
+}
+
